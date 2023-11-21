0473484f5ab33d0ec73bcc15faebe791ecea82bb
Gnosis <emmagab38@gmail.com>
2023-11-21 12:28:09 +0100

    commit

--- resources/views/emails/credit-alert.blade.php
+++ resources/views/emails/credit-alert.blade.php
@@ -82,11 +82,6 @@
                                         </td>
                                     </tr>
 
-                                    <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                                        <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                            Thank you for choosing <b>{{ env('APP_NAME') }}</b> as your trusted banking partner.
-                                        </td>
-                                    </tr>
                                     </tbody>
                                 </table>
                             </td>
